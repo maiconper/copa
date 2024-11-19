9c491af64d5e9927a2186ea24c6018eef448973a
maiconper <maicon.code.pereira@gmail.com>
2024-11-18 21:16:03 -0300

    commit inicial

---
+++ src/main/java/com/maicon/copa/CopaApplication.java
@@ -0,0 +1,13 @@
+package com.maicon.copa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopaApplication.class, args);
+	}
+
+}
